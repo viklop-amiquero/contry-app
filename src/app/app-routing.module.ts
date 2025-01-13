6bbaeb9e34a60e5533a471e16ae5dfd628003b1a
viklop <viklop7025@gmail.com>
2025-01-13 13:42:35 -0500

    configuración rutas básicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component'
+import { HomePageComponent } from './shared/pages/home-page/home-page.component'
+
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomePageComponent,
+    },
+    {
+        path: 'about',
+        component: AboutPageComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
