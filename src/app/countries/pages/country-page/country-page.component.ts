e2f114ebe4fcee302804bdb16dd198c902637dfe
viklop <viklop7025@gmail.com>
2025-01-14 13:25:07 -0500

    busqueda por capital, region y pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { CountriesService } from '../../services/countries.service'
+import { switchMap } from 'rxjs'
+import { Country } from '../../interfaces/countries.interface'
 
 @Component({
-  selector: 'app-country-page',
-  templateUrl: './country-page.component.html',
-  styleUrl: './country-page.component.css'
+    selector: 'app-country-page',
+    templateUrl: './country-page.component.html',
+    styleUrl: './country-page.component.css',
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+    public country?: Country
 
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _router: Router,
+        private _countriesService: CountriesService
+    ) {}
+
+    ngOnInit(): void {
+        this._activatedRoute.params
+            .pipe(
+                switchMap(({ id }) =>
+                    this._countriesService.searchCountryAlphaCode(id)
+                )
+            )
+            .subscribe((country) => {
+                if (!country) return this._router.navigateByUrl('')
+                return (this.country = country)
+            })
+    }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { catchError, Observable, of } from 'rxjs'
+import { catchError, Observable, of, map } from 'rxjs'
 import { Country } from '../interfaces/countries.interface'
 
 @Injectable({
@@ -10,6 +10,15 @@ export class CountriesService {
     private apiUrl: string = 'https://restcountries.com/v3.1'
 
     constructor(private _http: HttpClient) {}
+
+    searchCountryAlphaCode(code: string): Observable<Country | null> {
+        const url = `${this.apiUrl}/alpha/${code}`
+
+        return this._http.get<Country[]>(url).pipe(
+            map((countries) => (countries.length > 0 ? countries[0] : null)),
+            catchError(() => of(null))
+        )
+    }
 
     searchCapital(term: string): Observable<Country[]> {
         const url = `${this.apiUrl}/capital/${term}`
