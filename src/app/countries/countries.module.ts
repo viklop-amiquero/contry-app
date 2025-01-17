f2c6c1a51eca0ff2b34c4757fd919b2c2624fa94
viklop <viklop7025@gmail.com>
2025-01-17 09:53:08 -0500

    correcciones extras

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 
 import { ByCapitalPagesComponent } from './pages/by-capital-pages/by-capital-pages.component'
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component'
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component'
-import { CountryPageComponent } from './pages/country-page/country-page.component'
 import { CountriesRouting } from './countries-routing.module'
-import { SharedModule } from '../shared/shared.module';
+import { CountryPageComponent } from './pages/country-page/country-page.component'
 import { CountryTableComponent } from './components/country-table/country-table.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
     declarations: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,28 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 
 import { AboutPageComponent } from './pages/about-page/about-page.component'
-import { HomePageComponent } from './pages/home-page/home-page.component'
-import { SidebarComponent } from './components/sidebar/sidebar.component'
 import { ContactPageComponent } from './pages/contact-page/contact-page.component'
+import { HomePageComponent } from './pages/home-page/home-page.component'
 import { SearchBoxComponent } from './components/search-box/search-box.component'
+import { SidebarComponent } from './components/sidebar/sidebar.component'
 
 @NgModule({
     declarations: [
-        HomePageComponent,
         AboutPageComponent,
-        SidebarComponent,
         ContactPageComponent,
+        HomePageComponent,
         SearchBoxComponent,
+        SidebarComponent,
     ],
     imports: [CommonModule, RouterModule],
     exports: [
-        HomePageComponent,
         AboutPageComponent,
-        SidebarComponent,
         ContactPageComponent,
+        HomePageComponent,
         SearchBoxComponent,
+        SidebarComponent,
     ],
 })
 export class SharedModule {}
